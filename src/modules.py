727a653052e7bf5b0c0ab2c2788ac4ba47acb995
Tianyi Pan <tianyi.pan@clobotics.com>
2023-12-28 19:39:08 +0800

    Update prompts

--- src/modules.py
+++ src/modules.py
@@ -1,22 +1,29 @@
 MODULES = {
     "Finance": {
         "Financial Analysis": """<Template>
+
 SECTION: FINANCIAL ANALYSIS
 
 HEADER: OVERVIEW
+
 General summary of the overall financial status of the entity.
 
 HEADER: BALANCE SHEET
+
 Include one or several tables relevant for this subsection, followed by written details of the most recent key figures, changes, trends and other relevant information.
 
 HEADER: INCOME STATEMENT
+
 Include one or several tables relevant for this subsection, followed by written details of the most recent key figures, changes, trends and other relevant information.
 
 HEADER: CASH FLOW STATEMENT
+
 Include one or several tables relevant for this subsection, followed by written details of the most recent key figures, changes, trends and other relevant information.
 
 HEADER: KEY FINANCIAL METRICS
+
 Include one or several tables relevant for this subsection, followed by written details of the most recent key figures, changes, trends and other relevant information.
+
 </Template>""",
     },
 }
