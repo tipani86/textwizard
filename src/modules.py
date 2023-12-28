ddee2957902607df058f599b983e756405b5d403
Tianyi Pan <tianyi.pan@gmail.com>
2023-12-28 22:32:30 +0800

    Add Cover Letter template

--- src/modules.py
+++ src/modules.py
@@ -26,4 +26,33 @@ Include one or several tables relevant for this subsection, followed by written 
 
 </Template>""",
     },
+    "Job Search": {
+        "Cover Letter": """<Template>
+
+HEADER: CONTACT INFORMATION
+
+The first section or header includes your contact information: name, address, phone or cell phone number, and your email address. It has also become common to include your LinkedIn address so that employers can immediately access your professional profile, resume, and networking contacts. In an email, you can also list your contact info below your signature.
+
+HEADER: SALUTATION
+
+Getting a name to address your letter to is important. Avoid using the generic "To Whom It May Concern" or "Dear Sir or Madam," if you can find the recruiter or hiring manager's name.
+
+HEADER: OPENING PARAGRAPH
+
+Why you are writing. This is "the grab," your chance to grasp your reader by the collar and get their attention. Offer some specific, focused information regarding the job you're seeking and a few core strengths that demonstrate your suitability for the position.
+
+HEADER: BODY PARAGRAPH(S)
+
+Second paragraph: What you have to offer the employer. This is your hook where you highlight examples of the work performed and achieved results. Draw on your key competencies from your resume, although don't copy it word for word. Bullet points in this paragraph are extremely effective in drawing your reader's eye to your successes.
+
+Third paragraph: Your knowledge of the company. Show that you did your research and know something about the business and how you can contribute to its mission.
+
+HEADER: CLOSING PARAGRAPH
+
+Your closing. Summarize what you would bring to the position and suggest next steps by requesting a meeting or suggesting a call.
+
+Finish your letter with a formal closing like "Sincerely" or "Yours truly." If you're sending a paper letter, type your name after the salutation, leaving a space for your handwritten signature. If you're sending an email cover letter, type your name and contact information after your salutation. 
+
+</Template>""",
+    },
 }
